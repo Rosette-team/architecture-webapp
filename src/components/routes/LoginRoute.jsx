1649fe2575fafaf3b26a63746ae837951aba147c
BFDestroyeer <mzoreev@gmail.com>
2022-12-10 17:53:46 +0300

    Beautify login route

--- src/components/routes/LoginRoute.jsx
+++ src/components/routes/LoginRoute.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {UserDataStoreContext} from "../../index";
 import {observer} from "mobx-react";
+import './LoginRoute.css'
 import {useNavigate} from "react-router";
 import UserApi from "../../backend/api/UserApi";
 
@@ -32,12 +33,12 @@ function LoginRoute() {
     }
 
     return(
-        <div className="container-fluid">
+        <div className="container-fluid login-form">
             <form onSubmit={handleSubmit}>
-                <div className="form-group">
+                <div className="form-group m-2">
                     <input className="form-control" name="username" type="username" placeholder="Имя пользователя" onChange={e => setUsername(e.target.value)}/>
                 </div>
-                <div className="form-group mb-3">
+                <div className="form-group m-2">
                     <input className="form-control" name="password" type="password" placeholder="Пароль" onChange={e => setPassword(e.target.value)}/>
                 </div>
                 <button className="btn btn-primary m-1" type="submit">Войти</button>
